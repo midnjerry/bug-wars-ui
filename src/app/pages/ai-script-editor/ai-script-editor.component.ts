6cc70551d72395789fc627471551610a97d01e4a
Meagan Yannaccone <meagan@Josephs-MacBook-Pro.local>
2020-10-07 23:10:00 -0400

    Created script editor view

---
+++ src/app/pages/ai-script-editor/ai-script-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ai-script-editor',
+  templateUrl: './ai-script-editor.component.html',
+  styleUrls: ['./ai-script-editor.component.css']
+})
+export class AiScriptEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
