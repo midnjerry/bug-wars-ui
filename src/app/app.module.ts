f9491517ecefa6268ee4c898a62d94f5161680ef
Meagan Yannaccone <meagan@Josephs-MacBook-Pro.local>
2020-10-07 22:09:02 -0400

    register game lobby component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './pages/main-menu/main-menu.component';
+import { GameLobbyComponent } from './pages/game-lobby/game-lobby.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainMenuComponent
+    MainMenuComponent,
+    GameLobbyComponent
   ],
   imports: [
     BrowserModule,
