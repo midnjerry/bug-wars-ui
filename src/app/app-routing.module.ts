d074a5f6f2c7c329c31e931af952dece6f6feef3
Meagan Yannaccone <meagan@Josephs-MacBook-Pro.local>
2020-10-07 23:11:42 -0400

    Route AI script editor button from main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainMenuComponent } from './pages/main-menu/main-menu.component';
 import { GameLobbyComponent } from './pages/game-lobby/game-lobby.component';
+import { AiScriptEditorComponent } from './pages/ai-script-editor/ai-script-editor.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {path: 'home', component:MainMenuComponent},
-  {path: 'lobby', component:GameLobbyComponent}
+  {path: 'lobby', component:GameLobbyComponent},
+  {path: 'ai-editor', component:AiScriptEditorComponent}
 ];
 
 @NgModule({
