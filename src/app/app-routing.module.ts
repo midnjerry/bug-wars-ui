794eb6580b4ed6d5fd653497bbd57066a8fac4d4
Kellsey Neagley <kellseyneagley@gmail.com>
2020-10-07 20:15:51 -0500

    created the main menu view and buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainMenuComponent } from './pages/main-menu/main-menu.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {path: 'home', component:MainMenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
