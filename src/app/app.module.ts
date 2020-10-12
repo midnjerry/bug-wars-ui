939aaa5221962152274494104735c26933c28527
Kianek <kallridge92@gmail.com>
2020-10-12 10:10:18 -0700

    Add script editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainMenuComponent } from './pages/main-menu/main-menu.component';
 import { GameLobbyComponent } from './pages/game-lobby/game-lobby.component';
 import { AiScriptEditorComponent } from './pages/ai-script-editor/ai-script-editor.component';
+import { ScriptEditorComponent } from './components/script-editor/script-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainMenuComponent,
     GameLobbyComponent,
-    AiScriptEditorComponent
+    AiScriptEditorComponent,
+    ScriptEditorComponent
   ],
   imports: [
     BrowserModule,
