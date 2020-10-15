16843f8848a52624d3c6fbae6d05f5fa1a4753bb
Kellsey Neagley <kellseyneagley@gmail.com>
2020-10-14 21:22:57 -0500

    completed get all ai scripts test

--- src/app/models/aiscript.response.ts
+++ src/app/models/aiscript.response.ts
@@ -1,7 +1,8 @@
+import { AIScript } from '../models/aiscript';
 export class AIScriptResponse{
     ai: AIScript;
     error: string;
-    
+
     constructor(ai: AIScript, error: string) {
         this.ai = ai;
         this.error = error;
--- src/app/services/apiclient.service.ts
+++ src/app/services/apiclient.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { AIScript } from '../models/aiscript';
+import { AIScriptResponse} from '../models/aiscript.response';
+
 @Injectable({
   providedIn: 'root'
 })
 export class APIClientService {
   getAllAIScripts(){
     const url = environment.getAllAiScriptUrl;
-    response: AIScriptResponse = this.http.get(url);
-    return this.http.get(url)
+    // // response = this.http.get(url).subscribe()
+    // let response: AIScriptResponse;
+    // this.http.get(url).subscribe(res => {
+    //     // response =
+    //     this.getAllAIScripts();
+    // });
+
+    return this.http.get<AIScript[]>(url);
   }
   constructor(private http: HttpClient) { }
 }
