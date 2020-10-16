48416c1a1caefc2a45d874d5dbbf47f5d24a5bd5
Meagan Yannaccone <meagan@josephs-mbp.lan>
2020-10-16 18:15:11 -0400

    Added map-carousel to game lobby

--- src/app/pages/game-lobby/game-lobby.component.ts
+++ src/app/pages/game-lobby/game-lobby.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-lobby.component.css']
 })
 export class GameLobbyComponent implements OnInit {
-
+  public slides = [
+    { src: "https://incompetech.com/graphpaper/lite/1024%20boxes.png", desc: "map 1" },
+    { src: "https://incompetech.com/graphpaper/lite/1024%20boxes.png", desc: "map 2" },
+    { src: "https://incompetech.com/graphpaper/lite/1024%20boxes.png", desc: "map 3" }
+  ];
   constructor() { }
-
   ngOnInit(): void {
   }
-
 }
