bbd4d6627261c6671ce57d785bed2bc7f5004951
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-10-17 13:10:26 -0500

    WIP: initial commit

---
+++ src/main/java/codecamp/bug/wars/ui/BugWarsUiApplication.java
@@ -0,0 +1,16 @@
+package codecamp.bug.wars.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class BugWarsUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugWarsUiApplication.class, args);
+	}
+
+}
