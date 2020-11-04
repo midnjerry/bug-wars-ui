13b3f63a700c58027fa1747cb34bfe833f63c4ff
mlyannaccone <meagan@yannaccone.net>
2020-11-04 12:08:59 -0500

    fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { MainMenuComponent } from './pages/main-menu/main-menu.component';
 import { GameLobbyComponent } from './pages/game-lobby/game-lobby.component';
 import { AiScriptEditorComponent } from './pages/ai-script-editor/ai-script-editor.component';
 import { AboutComponent } from './pages/about/about.component';
+import { AiListComponent } from './pages/ai-list/ai-list.component';
 
 
 
@@ -11,9 +12,9 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {path: 'home', component:MainMenuComponent},
   {path: 'lobby', component:GameLobbyComponent},
-  {path: 'ai-editor', component:AiScriptEditorComponent}, 
-  {path: 'about', component:AboutComponent}
-  
+  {path: 'ai-editor', component:AiScriptEditorComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'ai-list', component:AiListComponent}
 ];
 
 @NgModule({
