e531451b73a67e957a7a39f2988dfbb3a8870d34
mlyannaccone <meagan@yannaccone.net>
2020-11-04 11:45:53 -0500

    created logic for and tested onSubmit method

--- src/app/components/script-editor/script-editor.component.spec.ts
+++ src/app/components/script-editor/script-editor.component.spec.ts
@@ -1,25 +1,30 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule} from '@angular/common/http/testing';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { ScriptEditorComponent } from './script-editor.component';
+import { APIClientService } from '../../services/apiclient.service';
 
 describe('ScriptEditorComponent', () => {
   let component: ScriptEditorComponent;
   let fixture: ComponentFixture<ScriptEditorComponent>;
+  let apiClient: APIClientService;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        HttpClientTestingModule,
-        FormsModule,
-        ReactiveFormsModule
-      ],
-      declarations: [ ScriptEditorComponent ]
-    })
-    .compileComponents();
-  }));
+  beforeEach(() => {
+    TestBed.configureTestingModule({ imports: [
+      HttpClientTestingModule,
+      FormsModule,
+      ReactiveFormsModule
+    ],
+    declarations: [
+      ScriptEditorComponent
+    ]
+    }).compileComponents;
+
+    fixture = TestBed.createComponent(ScriptEditorComponent);
+    apiClient = TestBed.inject(APIClientService);
+    component = fixture.componentInstance;
+    component.ngOnInit();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ScriptEditorComponent);
@@ -30,5 +35,34 @@ describe('ScriptEditorComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('onSubmit()', () => {
+    it('should call updateAIScript() when scriptId is set', () => {
+      //arrange
+      let id = component.formScript.controls['scriptId']
+      id.setValue(1);
+      let updateSpy = spyOn(apiClient, 'updateAIScript').and.callThrough();
+
+      //act
+      component.onSubmit();
+
+      //assert
+      expect(updateSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('onSubmit()', () => {
+    it('should call createAIScript() when scriptId is not set', () => {
+      //arrange
+      let updateSpy = spyOn(apiClient, 'createAIScript').and.callThrough();
+
+      //act
+      component.onSubmit();
+
+      //assert
+      expect(updateSpy).toHaveBeenCalled();
+    });
+  });
+
 });
 
