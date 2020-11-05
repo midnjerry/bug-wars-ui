faff31ae7d63deaf924933721ac11114bb1f56ef
Kianek <kallridge92@gmail.com>
2020-11-05 08:06:49 -0800

    Initial commit

---
+++ src/app/components/bug-dropdown/bug-dropdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bug-dropdown',
+  templateUrl: './bug-dropdown.component.html',
+  styleUrls: ['./bug-dropdown.component.css']
+})
+export class BugDropdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
