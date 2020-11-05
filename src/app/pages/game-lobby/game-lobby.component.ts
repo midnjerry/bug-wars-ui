8c989796be9b5dfb7ce51a7bc668273a473053eb
Kianek <kallridge92@gmail.com>
2020-11-05 08:09:20 -0800

    Format and remove font awesome references

--- src/app/pages/game-lobby/game-lobby.component.ts
+++ src/app/pages/game-lobby/game-lobby.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { faBug } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-game-lobby',
   templateUrl: './game-lobby.component.html',
-  styleUrls: ['./game-lobby.component.css']
+  styleUrls: ['./game-lobby.component.css'],
 })
 export class GameLobbyComponent implements OnInit {
-  faBug = faBug;
   public slides = [
-    { src: "https://incompetech.com/graphpaper/lite/1024%20boxes.png", desc: "map 1" },
-    { src: "https://incompetech.com/graphpaper/lite/1024%20boxes.png", desc: "map 2" },
-    { src: "https://incompetech.com/graphpaper/lite/1024%20boxes.png", desc: "map 3" }
+    {
+      src: 'https://incompetech.com/graphpaper/lite/1024%20boxes.png',
+      desc: 'map 1',
+    },
+    {
+      src: 'https://incompetech.com/graphpaper/lite/1024%20boxes.png',
+      desc: 'map 2',
+    },
+    {
+      src: 'https://incompetech.com/graphpaper/lite/1024%20boxes.png',
+      desc: 'map 3',
+    },
   ];
-  constructor() { }
-  ngOnInit(): void {
-  }
+  constructor() {}
+  ngOnInit(): void {}
 }
