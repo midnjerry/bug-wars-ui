49dc698772f3cce7c3e63c72209993bdd32f528b
Kianek <kallridge92@gmail.com>
2020-11-05 08:14:01 -0800

    Add color and options

--- src/app/components/bug-dropdown/bug-dropdown.component.ts
+++ src/app/components/bug-dropdown/bug-dropdown.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bug-dropdown',
   templateUrl: './bug-dropdown.component.html',
-  styleUrls: ['./bug-dropdown.component.css']
+  styleUrls: ['./bug-dropdown.component.css'],
 })
 export class BugDropdownComponent implements OnInit {
+  @Input() color: string;
+  @Input() options: [string];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
