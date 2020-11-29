7567f15a81d6b7d270dc6c38e67184279025e9db
Alec Stewart <alec-stewart@protonmail.com>
2020-11-29 13:31:51 -0600

    Have ground tile image render inside of canvas

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,37 +1,43 @@
 import { MapData } from './../../models/map-data';
-import { ViewChild, ElementRef, Input, AfterViewInit } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
-import { ImageAlt } from 'ngx-bootstrap-icons';
+import {
+  Component,
+  Input,
+  ElementRef,
+  ViewChild,
+  OnInit,
+  AfterViewInit,
+} from '@angular/core';
+import { ImageAlt, Square } from 'ngx-bootstrap-icons';
 import { APIClientService } from 'src/app/services/apiclient.service';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
 })
+
 export class MapComponent implements OnInit, AfterViewInit {
   @Input() mapData: MapData;
-  @ViewChild('canvas', { static: true })
-  canvas: HTMLCanvasElement;
+  @ViewChild('canvas', { static: false })
+  canvas: ElementRef<HTMLCanvasElement>;
 
   private ctx: CanvasRenderingContext2D;
   ground1 = new Image();
   ground2 = new Image();
   block = new Image();
+  ground_img = new Image();
 
   constructor(private apiClient: APIClientService) { }
 
   ngOnInit(): void {
-    this.ground1.src = "./assets/images/tiles/ground/Ground_Tile_01_C.png";
-    this.ground2.src = "./assets/images/tiles/ground/Ground_Tile_02_C.png";
-    this.block.src = "./assets/images/tiles/blocks/Block_A_02.png";
-
-    this.apiClient.getMap(1).subscribe((response) => this.mapData = response.body);
+    this.apiClient
+      .getMap(1)
+      .subscribe((response) => (this.mapData = response.body));
   }
 
   ngAfterViewInit(): void {
-    this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
-    this.ctx = this.canvas.getContext('2d');
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    this.verifyCanvas();
     this.displayMap();
   }
 
@@ -40,26 +46,53 @@ export class MapComponent implements OnInit, AfterViewInit {
     this.displayWall(this.ctx);
   }
 
+  verifyCanvas(): void {
+    if (this.ctx) {
+      console.log('yes');
+    } else {
+      console.log('no')
+    }
+  }
+
+  /// TODO properly calculate x and y position for block(s) overlayed on
+  /// ground tile image in canvas
   displayWall(context): void {
     for (var i = 0; i < 6; i++) {
       for (var j = 0; j < 6; j++) {
         if (i == 0 || j == 0 || i == 5 || j == 5 || this.getRandomIndex(10) == 0) {
-          context.drawImage(this.block, 128 * i, 128 * j);
+          let x: number = 128 * i;
+          let y: number = 128 * j;
+          this.block.src = "./assets/images/tiles/blocks/Block_A_02.png";
+          this.block.onload = (): void => {
+            context.drawImage(this.block, x, y);
+          }
         }
       }
     }
   }
 
+  /// TODO properly calculate the width (w) and height (h) for image inside of canvas
+  /// based on resolution of screen
   displayFloor(context): void {
     for (var i = 0; i < 3; i++) {
       for (var j = 0; j < 3; j++) {
         switch (this.getRandomIndex(2)) {
           case 0: {
-            context.drawImage(this.ground1, 256 * i, 256 * j);
+            let w: number = 256 * i;
+            let h: number = 256 * j;
+            this.ground1.src = "./assets/images/tiles/ground/Ground_Tile_01_C.png";
+            this.ground1.onload = (): void => {
+              context.drawImage(this.ground1, 0, 0, w, h);
+            }
             break;
           }
           case 1: {
-            context.drawImage(this.ground2, 256 * i, 256 * j);
+            let w: number = 256 * i;
+            let h: number = 256 * j;
+            this.ground2.src = "./assets/images/tiles/ground/Ground_Tile_02_C.png";
+            this.ground2.onload = (): void => {
+              context.drawImage(this.ground2, 0, 0, w, h);
+            }
             break;
           }
         }
@@ -70,4 +103,5 @@ export class MapComponent implements OnInit, AfterViewInit {
   getRandomIndex(length: number): number {
     return Math.floor(Math.random() * length);
   }
+
 }
